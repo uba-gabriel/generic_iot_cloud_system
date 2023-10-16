d8e711ac90f1cbcf9f3ade0513217b39eb12ca90
uba-gabriel <gabrielcarutti@gmail.com>
2023-10-16 14:53:52 -0300

    Detalles de configuración

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -12,13 +12,11 @@ export class LoginComponent {
   password: string;
 
   onSubmit() {
-    // Aquí puedes implementar la lógica para enviar los datos del formulario al servidor y manejar la respuesta.
-    // Por ejemplo, puedes utilizar un servicio de autenticación para verificar las credenciales.
+    // Utilizaré un servicio de autenticación para verificar las credenciales.
 
     // Aquí, simplemente mostraremos un mensaje en la consola con los datos del formulario.
     console.log('Usuario:', this.username);
     console.log('Contraseña:', this.password);
 
-    // Luego, podrías redirigir al usuario a otra página si el inicio de sesión es exitoso.
   }
 }
